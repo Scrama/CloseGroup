3b29b755432cc3ad0e94fa28482e91be2ee7b02a
Dmitry Navrotsky <dnavrotsky@plazius.com>
2019-10-21 10:14:07 +0300

    Тесты

--- CloseGroup.Tests/CloseGroupTests.cs
+++ CloseGroup.Tests/CloseGroupTests.cs
@@ -1,33 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System;
 
 namespace CloseGroup.Tests
 {
-    public class TestData
-    {
-        public (string input, string output, int cnt)[] InputProductNames =
-        {
-            ("Шоколадный батончик Сникерс", "шоколадный батончик сникерс", 3),
-            ("Стиральный порошок Сказка", "стиральный порошок сказка", 3)
-        };
-    }
     [TestFixture]
     public class CloseGroupTests
     {
-        [TestCaseSource(nameof(ProductNamesCases))]
-        public void ProductNameProcessorTest((string input, string output, int cnt) casePair)
+        private readonly ISettings settings = new TestSettings();
+
+        [TestCaseSource(nameof(InputProductNames))]
+        public void ProductNameProcessorTest((string input, string output, int cnt) caseData)
         {
             var pnp = new ProductNameProcessor();
-            var list = pnp.Process(casePair.input);
-            Assert.AreEqual(list.Count, casePair.cnt);
+            var list = pnp.Process(caseData.input);
+            var expected = caseData.output.Split(" ");
+            Assert.AreEqual(caseData.cnt, list.Count);
+            for (var i = 0; i < 3; ++i)
+            {
+                Assert.AreEqual(expected[i], list[i]);
+            }
         }
 
-        private static IEnumerable<(string input, string output, int cnt)> ProductNamesCases()
+        [TestCaseSource(nameof(SimilarWords))]
+        public void SimilarWordTest((string first, string second, bool isSimilar) caseData)
         {
-            foreach (var tuple in InputProductNames)
+            var result = WordUtils.IsWordsSimilar(caseData.first, caseData.second, 0.3);
+            Assert.AreEqual(caseData.isSimilar, result);
+        }
+
+        [Test]
+        public void KeyWordAnlyzerTest()
+        {
+            var repo = new TestRepo();
+            var pnp = new ProductNameProcessor();
+            var kwa = new KeyWordAnalyzer(repo, pnp, settings);
+
+            kwa.Analyze(true);
+
+            Assert.AreEqual(2, repo.List()[0].KeyWords.Count);
+            Assert.AreEqual(2, repo.List()[1].KeyWords.Count);
+        }
+
+        [Test]
+        public void DirectProductTest()
+        {
+            var repo = new TestRepo();
+            var pnp = new ProductNameProcessor();
+            var kwa = new KeyWordAnalyzer(repo, pnp, settings);
+            var svc = new CloseGroupService(settings, repo, pnp, kwa);
+
+            var result = svc.CloseGroupFor("шоколадный батончик сникерс");
+            Assert.AreEqual("Шоколадные батончики", result);
+        }
+
+        [Test]
+        public void CloseProductTest()
+        {
+            var repo = new TestRepo();
+            var pnp = new ProductNameProcessor();
+            var kwa = new KeyWordAnalyzer(repo, pnp, settings);
+            var svc = new CloseGroupService(settings, repo, pnp, kwa);
+
+            var result = svc.CloseGroupFor("шоколадный батончик сказка");
+            Assert.AreEqual("Шоколадные батончики", result);
+        }
+
+        [Test]
+        public void CloseProductTestFail()
+        {
+            var repo = new TestRepo();
+            var pnp = new ProductNameProcessor();
+            var kwa = new KeyWordAnalyzer(repo, pnp, settings);
+            var svc = new CloseGroupService(settings, repo, pnp, kwa);
+
+            var productName = "большая простыня";
+            try
             {
-                yield return tuple;
+                svc.CloseGroupFor(productName);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual($"Не удалось определить группу для {productName}", e.Message);
             }
         }
 
@@ -35,6 +88,12 @@ namespace CloseGroup.Tests
         {
             ("Шоколадный батончик Сникерс", "шоколадный батончик сникерс", 3),
             ("Стиральный порошок Сказка", "стиральный порошок сказка", 3)
+        };
+
+        private static readonly (string first, string second, bool isSimilar)[] SimilarWords =
+        {
+            ("шоколадный", "шоколадная", true ),
+            ("порошок", "сникерс", false)
         };
     }
 }
